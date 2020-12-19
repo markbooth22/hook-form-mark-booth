3ad83cd0783230efccb9b50fc2ee784e01b96479
Mark Booth <markbooth22@gmail.com>
2020-12-18 17:10:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import UserForm from './components/UserForm'
+import Results from './components/Results'
+import React, { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserForm inputs={state} setInputs={setState}/>
+      <Results data={state}/>
     </div>
   );
 }
---
+++ src/components/Results.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Results = (props) =>{
+    const { firstName, lastName, email, password, confirmPassword } = props.data;
+    return(
+        <div>
+            <p>Your Form Data</p>
+            <p>First Name: { firstName }</p>
+            <p>Last Name: { lastName }</p>
+            <p>Email: { email }</p>
+            <p>Password: { password }</p>
+            <p>Confirm Password: { confirmPassword }</p>
+        </div>
+    );
+};
+
+export default Results;
